dbd11b00956ed98f0f1fd75b24d1c72001dd1d86
Rafael Reis <rafaelrnascimento2006@gmail.com>
2021-09-29 09:45:32 -0300

    desafio feito

---
+++ src/types.ts
@@ -0,0 +1,15 @@
+export interface IFood {
+  id: number;
+  name: string;
+  description: string;
+  price: string;
+  available: boolean;
+  image: string;
+}
+
+export interface ModalFood {
+  name: string;
+  image: string;
+  price: string;
+  description: string;
+}
